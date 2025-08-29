6648e0707d4da1fd09eaa52f6f2b1b2c850c1033
Farhat Baaroun <69281333+GldzzPro@users.noreply.github.com>
2025-08-29 14:39:20 +0100

    Revert "fix some style"

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { LandingPage } from "./screens/LandingPage/LandingPage";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <LandingPage />
-    </BrowserRouter>
-  </StrictMode>
+    <LandingPage />
+  </StrictMode>,
 );
