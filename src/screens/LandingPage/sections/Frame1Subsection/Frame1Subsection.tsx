1aeb373c30ca671ef99ebb862c31c0bf42ff2c69
GldzzPro <golden.farhat@gmail.com>
2025-08-30 18:10:05 +0100

    feat(i18n): update feature descriptions in English and French locales for clarity and consistency; enhance layout in Frame1Subsection component

--- src/screens/LandingPage/sections/Frame1Subsection/Frame1Subsection.tsx
+++ src/screens/LandingPage/sections/Frame1Subsection/Frame1Subsection.tsx
@@ -1,13 +1,11 @@
-import { ArrowRightIcon } from "lucide-react";
-import { Button } from "../../../../components/ui/button";
 import { useTranslation } from "react-i18next";
 
 export const Frame1Subsection = (): JSX.Element => {
   const { t } = useTranslation("demo");
 
   return (
-    <section className="w-full p-6 bg-[#3879F0] rounded-3xl inline-flex flex-col md:flex-row justify-start items-center gap-2.5">
-      <div className="flex-1 justify-start">
+    <section className="w-full p-[25px] bg-[#3879F0] rounded-3xl inline-flex flex-col md:flex-row justify-start items-start md:items-center gap-2.5">
+      <div className="self-stretch justify-start">
         <span className="text-white text-base font-medium font-['Quicksand'] leading-normal mr-1">
           {t("description.part1")}{" "}
         </span>
@@ -18,15 +16,13 @@ export const Frame1Subsection = (): JSX.Element => {
           {t("description.part3")}
         </span>
       </div>
-      <div className="h-8 px-2.5 py-[5px] bg-[#DEEAFF] rounded-[20px] md:flex inline-flex justify-center items-center gap-2.5 hover:bg-[#3879F0]">
-        <div className="flex hover:text-white text-[#3879F0] items-center gap-1.5 md:gap-2.5 text-center justify-start text-blue-500 text-sm font-medium font-['Quicksand'] leading-tight">
-          {t("button")}
-          <img
-            className="w-[24px] h-[24px] mt-[-3.95px] mb-[-3.95px]"
-            alt="Vuesax linear arrow"
-            src="/vuesax-linear-arrow-right.svg"
-          />
-        </div>
+      <div className="flex h-8.5 w-[150px] px-[10px] py-[5px] box-border hover:bg-transparent hover:text-[#DEEAFF] rounded-[20px] bg-[#DEEAFF]  justify-center items-center gap-1.5   text-sm font-medium font-['Quicksand'] leading-tight">
+        {t("button")}
+        <img
+          className="w-[24px] h-[24px] mt-[-3.95px] mb-[-3.95px]"
+          alt="Vuesax linear arrow"
+          src="/vuesax-linear-arrow-right.svg"
+        />
       </div>
     </section>
   );
